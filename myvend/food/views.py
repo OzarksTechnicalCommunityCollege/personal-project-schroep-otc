81148694efbe1e6be94d812e55bd7994c7bcd112
Stephen <schroeps@otc.edu>
2026-02-03 13:23:31 -0600

    added templates, form, and views

--- myvend/food/views.py
+++ myvend/food/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import FoodEnterForm
 
 # Create your views here.
+
+# Render food entry form into a view
+def enter_view(request):
+    context = {}
+    context['form'] = FoodEnterForm
+    return render(request, "home.html", context)
+
+# Define "about" view
+def about_view(request):
+    return(render(request, 'about.html'))
