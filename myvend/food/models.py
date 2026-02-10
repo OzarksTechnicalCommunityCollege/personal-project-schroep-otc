018303f9df8549a1a3d6f5c24c6117f0a0e2ee7a
SCHROEPPEL, STEPHEN C. <ss0102429@otc.edu>
2026-02-09 20:03:59 -0600

    updated models.py

--- myvend/food/models.py
+++ myvend/food/models.py
@@ -1,19 +1,22 @@
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 
-# Create Food model 
-class Food(models.Model):
-    name = models.CharField(max_length=50)
-    calories = models.PositiveIntegerField() # using PositiveIntegerField since I want it to be 0 or positive
-    fat = models.PositiveIntegerField()
-    protein = models.PositiveIntegerField()
-    fiber = models.PositiveIntegerField()
-    purine = models.PositiveIntegerField()
-    location = models.CharField(max_length=50)
-    curr_date = models.DateTimeField(default=timezone.now)
+# Create FoodItem model 
+class FoodItem(models.Model):
+    food_name = models.CharField(max_length=50)
+    type_id = models.PositiveIntegerField() # using PositiveIntegerField since I want it to be 0 or positive
+    location_id = models.PositiveIntegerField()
+    expiry_date = models.DateField()
     quantity = models.PositiveIntegerField()
-    updated = models.DateTimeField(auto_now=True)
 
+# Create FoodType model
+class FoodType(models.Model):
+    food_type = models.CharField(max_length=50)
+
+# Create Location model
+class Location(models.Model):
+    location = models.CharField(max_length=50)
+    
 
     def __str__(self):
         return self.name
