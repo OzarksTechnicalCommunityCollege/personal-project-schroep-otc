25667620fb1407b66f19e2c4482b9f977c8dab6e
Stephen <schroeps@otc.edu>
2026-02-12 12:38:00 -0600

    admin update 2

--- myvend/food/admin.py
+++ myvend/food/admin.py
@@ -6,11 +6,14 @@ from.models import FoodItem, FoodType, Location
 
 
 # Register your models here.
-admin.site.register(FoodItem)
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ["food_name", "food_type", "location", "quantity", "expiry_date"]
+    list_filter = ["food_type", "location"]
+    search_fields = ["food_name"]
+    ordering = ["food_name"]
+    show_facets = admin.ShowFacets.ALWAYS
+
+
 admin.site.register(FoodType)
 admin.site.register(Location)
-class FoodAdmin(admin.ModelAdmin):
-    list_display = ['name', 'location', 'calories','purine','protein', 'fat', 'fiber', 'quantity']
-    list_filter = ['name', 'location']
-    search_fields = ['name', 'location', 'calories','purine','protein', 'fat', 'fiber']
-    show_facets = admin.ShowFacets.ALWAYS
