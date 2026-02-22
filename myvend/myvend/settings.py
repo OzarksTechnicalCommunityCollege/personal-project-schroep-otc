afa63c3f698b81c2f7aa3b8576dcb4652898ac01
Stephen <schroeps@otc.edu>
2026-02-21 21:52:08 -0600

    hardcoding database to bypass decouple

--- myvend/myvend/settings.py
+++ myvend/myvend/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'myvend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME': 'food',
+        'USER': 'food',
+        'PASSWORD': 'Docker2',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
