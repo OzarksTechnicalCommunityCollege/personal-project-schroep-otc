a3964b055235cd2fb0202b26a8f7950c73ae04b8
Stephen <schroeps@otc.edu>
2026-02-21 22:11:08 -0600

    hardcoded new password

--- myvend/myvend/settings.py
+++ myvend/myvend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'food',
         'USER': 'food',
-        'PASSWORD': 'Docker2',
+        'PASSWORD': 'food123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
