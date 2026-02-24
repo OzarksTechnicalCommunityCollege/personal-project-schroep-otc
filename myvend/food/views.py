65f88f1b50286cac16a37e3c9ba5d84735fdb76d
Stephen <schroeps@otc.edu>
2026-02-24 14:51:49 -0600

    created login view

--- myvend/food/views.py
+++ myvend/food/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import FoodItem
 from django.contrib.postgres.search import SearchVector
-from .forms import SearchForm
+from .forms import SearchForm, LoginForm
 
 # Create your views here.
 
@@ -44,3 +46,26 @@ def food_search(request):
             'results': results
         }
     )
+
+# Define login view
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
+            if user is not NONE:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
