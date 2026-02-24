b70dbd65ff98f2c2da39606515cf831a01890143
Stephen <schroeps@otc.edu>
2026-02-23 20:28:56 -0600

    created custom filter and added search.html file

---
+++ myvend/food/templatetags/food_extras.py
@@ -0,0 +1,14 @@
+from django import template
+from django.utils.safestring import mark_safe
+import markdown as md
+import bleach # strips any dangerous html from input before marked safe
+
+register = template.Library()
+
+ALLOWED_TAGS = bleach.sanitizer.ALLOWED_TAGS + [
+    'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+]
+
+@register.filter(name='markdown')
+def markdown(value):
+    return mark_safe(md.markdown(value))
