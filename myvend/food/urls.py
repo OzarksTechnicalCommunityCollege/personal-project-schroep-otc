3bddc716010cbc80d6f69ff64f86b0298576b4d6
Stephen <schroeps@otc.edu>
2026-02-24 15:03:41 -0600

    added login path to apps urls.py

--- myvend/food/urls.py
+++ myvend/food/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.fooditem_list, name="home"),
     path("about/", views.about_view, name="about"),
     path("search/", views.food_search, name="food_search"),
+    path("login/", views.user_login, name="login"),
 ]
