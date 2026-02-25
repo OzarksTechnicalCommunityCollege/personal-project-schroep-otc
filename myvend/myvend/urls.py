27302d24ba78e0a98afdf80f93193add99ed66be
Stephen <schroeps@otc.edu>
2026-02-24 18:47:54 -0600

    testing authent

--- myvend/myvend/urls.py
+++ myvend/myvend/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("food.urls")),
-    path("account/", include("accounts.urls")),
+    #path("account/", include("accounts.urls")),
 ]
