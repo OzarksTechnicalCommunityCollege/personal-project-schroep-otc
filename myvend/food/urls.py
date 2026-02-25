a18c3cbba2e2136da3e3c279b0741c1e7e9b5c13
Stephen <schroeps@otc.edu>
2026-02-24 19:35:36 -0600

    created a login and logout template under registration

--- myvend/food/urls.py
+++ myvend/food/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -7,5 +8,7 @@ urlpatterns = [
     path("", views.fooditem_list, name="home"),
     path("about/", views.about_view, name="about"),
     path("search/", views.food_search, name="food_search"),
-    path("login/", views.user_login, name="login"),
+    #path("login/", views.user_login, name="login"), #previous login path
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
