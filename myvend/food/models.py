b5e7d603136d551d11bc0a37ff8385ecfe91cd18
Stephen <schroeps@otc.edu>
2026-03-12 12:02:40 -0500

    added FoodItemTag model

--- myvend/food/models.py
+++ myvend/food/models.py
@@ -21,6 +21,17 @@ class FoodItem(models.Model):
     location = models.ForeignKey(Location, on_delete=models.PROTECT, related_name="items")
     expiry_date = models.DateField()
     quantity = models.PositiveIntegerField(default=1)
+    tags = models.ManyToManyField(Tag, through="FoodItemTag", related_name="food_items")
 
     def __str__(self):
         return f"{self.food_name} ({self.quantity})"
+    
+# Create FoodItemTag model
+class FoodItemTag(models.Model):
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tagged_on = models.DateField(auto_now_add=True)
+    tagged_by = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        unique_together = ("food_item", "tag")
