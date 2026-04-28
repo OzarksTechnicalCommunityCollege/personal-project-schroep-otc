4d287e0d41c316854b69e9f113db9f0c115113f8
Stephen <schroeps@otc.edu>
2026-04-28 12:46:57 -0500

    added nutritional model

--- myvend/food/models.py
+++ myvend/food/models.py
@@ -46,3 +46,26 @@ class FoodItemTag(models.Model):
 
     def __str__(self):
         return f"{self.food_item} — {self.tag}"
+
+# Create NutritionInfo model
+class NutritionInfo(models.Model):
+    food_item = models.OneToOneField(
+        FoodItem,
+        on_delete=models.CASCADE,
+        related_name="nutrition"
+    )
+
+    fdc_id = models.IntegerField(blank=True, null=True)
+    calories = models.FloatField(blank=True, null=True)
+    protein_g = models.FloatField(blank=True, null=True)
+    carbs_g = models.FloatField(blank=True, null=True)
+    fat_g = models.FloatField(blank=True, null=True)
+    fiber_g = models.FloatField(blank=True, null=True)
+    sugar_g = models.FloatField(blank=True, null=True)
+    sodium_mg = models.FloatField(blank=True, null=True)
+
+    serving_size = models.FloatField(blank=True, null=True)
+    serving_unit = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"Nutrition for {self.food_item.food_name}"
--- myvend/myvend/settings.py
+++ myvend/myvend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+
+# USDA API Key
+USDA_API_KEY = os.environ.get("USDA_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
